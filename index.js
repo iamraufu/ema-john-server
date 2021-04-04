c292b9af8a1d42adfe256e8c43b9c13e98974199
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-04 14:51:47 +0600

    all products added to database

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@raufuprezensinc.hztjo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
 const app = express()
-const port = 5000
 
-app.get('/', (req, res) => {
-    res.send('Server is Running!')
-})
+app.use(bodyParser.json());
+app.use(cors());
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+const port = 5000;
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const productsCollection = client.db("emaJohn").collection("products");
+    const ordersCollection = client.db("emaJohn").collection("orders");
+
+    app.post('/addProduct', (req, res) => {
+        const products = req.body;
+        // productsCollection.insertOne(products)
+        //     .then(result => {
+        //         console.log(result.insertedCount);
+        //         res.send(result.insertedCount)
+        //     })
+        productsCollection.insertMany(products)
+            .then(result => {
+                console.log(result.insertedCount);
+                res.send(200).send(result.insertedCount)
+            })
+
+    })
+
+    // app.get('/products', (req, res) => {
+    //     productsCollection.find({})
+    //         .toArray((err, documents) => {
+    //             res.send(documents);
+    //         })
+    // })
+
+    // app.get('/product/:key', (req, res) => {
+    //     productsCollection.find({ key: req.params.key })
+    //         .toArray((err, documents) => {
+    //             res.send(documents[0]);
+    //         })
+    // })
+
+    // app.post('/productsByKeys', (req, res) => {
+    //     const productKeys = req.body;
+    //     productsCollection.find({ key: { $in: productKeys } })
+    //         .toArray((err, documents) => {
+    //             res.send(documents);
+    //         })
+    // })
+
+    // app.post('/addOrder', (req, res) => {
+    //     const order = req.body;
+    //     ordersCollection.insertOne(order)
+    //         .then(result => {
+    //             res.send(result.insertedCount > 0)
+    //         })
+    // })
+
+});
+
+
+app.listen(port)
