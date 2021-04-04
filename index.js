35f2db737f2bf4c1cebb3c40cbec67aa5d27bb1c
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-04 15:58:32 +0600

    data loaded from database

--- index.js
+++ index.js
@@ -37,19 +37,19 @@ client.connect(err => {
 
     })
 
-    // app.get('/products', (req, res) => {
-    //     productsCollection.find({})
-    //         .toArray((err, documents) => {
-    //             res.send(documents);
-    //         })
-    // })
+    app.get('/products', (req, res) => {
+        productsCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
 
-    // app.get('/product/:key', (req, res) => {
-    //     productsCollection.find({ key: req.params.key })
-    //         .toArray((err, documents) => {
-    //             res.send(documents[0]);
-    //         })
-    // })
+    app.get('/product/:key', (req, res) => {
+        productsCollection.find({ key: req.params.key })
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            })
+    })
 
     // app.post('/productsByKeys', (req, res) => {
     //     const productKeys = req.body;
